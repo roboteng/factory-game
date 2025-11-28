3df5fc7a352371b025864260be5555b006cc50bb
Trevor Settles <roboteng@gmail.com>
2025-11-27 22:17:51 -0700

    checked for item on conveyor

--- src/game.rs
+++ src/game.rs
@@ -4,6 +4,7 @@ pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, (startup, startup_camera));
+        app.add_systems(Update, conveyor_moves_items);
     }
 }
 
@@ -48,9 +49,24 @@ impl Conveyor {
     }
 }
 
-fn conveyor_moves_items(items: Query<&mut Transform, With<WorldItem>>) {
+fn conveyor_moves_items(
+    conveyors: Query<(&Conveyor, &Transform)>,
+    items: Query<&mut Transform, (With<WorldItem>, Without<Conveyor>)>,
+) {
     for mut item in items {
-        item.translation.x += 1.0;
+        for (conveyor, con_trans) in conveyors {
+            if (item.translation.x - con_trans.translation.x).abs() < 16.0 {
+                match conveyor.direction {
+                    Direction::North => {
+                        item.translation.y += 1.0;
+                    }
+                    Direction::East => {
+                        item.translation.x += 1.0;
+                    }
+                    _ => todo!(),
+                }
+            }
+        }
     }
 }
 
@@ -63,8 +79,25 @@ mod tests {
         cmd.spawn((WorldItem, Transform::default()));
     }
 
-    fn place_conveyor(mut cmd: Commands) {
+    fn place_ore_offset(mut cmd: Commands) {
+        cmd.spawn((
+            WorldItem,
+            Transform {
+                translation: Vec3 {
+                    x: 32.0,
+                    ..Vec3::default()
+                },
+                ..Transform::default()
+            },
+        ));
+    }
+
+    fn place_conveyor_east(mut cmd: Commands) {
         cmd.spawn((Conveyor::new(Direction::East), Transform::default()));
+    }
+
+    fn place_conveyor_north(mut cmd: Commands) {
+        cmd.spawn((Conveyor::new(Direction::North), Transform::default()));
     }
 
     #[test]
@@ -85,7 +118,7 @@ mod tests {
         let mut app = App::new();
         app.add_plugins(MinimalPlugins);
         app.add_systems(Startup, place_ore);
-        app.add_systems(Startup, place_conveyor);
+        app.add_systems(Startup, place_conveyor_east);
         app.add_systems(Update, conveyor_moves_items);
 
         app.update();
@@ -95,5 +128,39 @@ mod tests {
         assert_eq!(1, items.len());
         let actual_x = items[0].1.translation.x;
         assert!(actual_x > 0.0, "expected {actual_x} to be bigger than 0.0");
+    }
+
+    #[test]
+    fn conveyor_moves_item_north() {
+        let mut app = App::new();
+        app.add_plugins(MinimalPlugins);
+        app.add_systems(Startup, place_ore);
+        app.add_systems(Startup, place_conveyor_north);
+        app.add_systems(Update, conveyor_moves_items);
+
+        app.update();
+
+        let mut query = app.world_mut().query::<(&WorldItem, &Transform)>();
+        let items = query.iter(app.world()).collect::<Vec<_>>();
+        assert_eq!(1, items.len());
+        let actual_y = items[0].1.translation.y;
+        assert!(actual_y > 0.0, "expected {actual_y} to be bigger than 0.0");
+    }
+
+    #[test]
+    fn conveyor_doesnt_moves_item() {
+        let mut app = App::new();
+        app.add_plugins(MinimalPlugins);
+        app.add_systems(Startup, place_ore_offset);
+        app.add_systems(Startup, place_conveyor_north);
+        app.add_systems(Update, conveyor_moves_items);
+
+        app.update();
+
+        let mut query = app.world_mut().query::<(&WorldItem, &Transform)>();
+        let items = query.iter(app.world()).collect::<Vec<_>>();
+        assert_eq!(1, items.len());
+        let actual_y = items[0].1.translation.y;
+        assert_eq!(actual_y, 0.0);
     }
 }
