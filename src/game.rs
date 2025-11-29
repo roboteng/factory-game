9999963f71661b5bf196b4a73222e3ddf85c115c
Trevor Settles <roboteng@gmail.com>
2025-11-28 20:50:57 -0700

    prevented movement if not on conveyor belt

--- src/game.rs
+++ src/game.rs
@@ -120,19 +120,19 @@ fn conveyor_moves_items(
 ) {
     for mut item in items {
         for (conveyor, con_trans) in conveyors {
-            let rel_x = item.translation.x - con_trans.translation.x;
-            if rel_x < 3.0 * TILE_SIZE / 4.0 {
-                match conveyor.direction {
-                    Direction::North => {
-                        item.translation.y += CONVEYOR_BASE_SPEED * time.delta_secs();
-                    }
-                    Direction::East => {
+            match conveyor.direction {
+                Direction::North => {
+                    item.translation.y += CONVEYOR_BASE_SPEED * time.delta_secs();
+                }
+                Direction::East => {
+                    let rel_x = item.translation.x - con_trans.translation.x;
+                    if rel_x.abs() < 3.0 * TILE_SIZE / 4.0 {
                         item.translation.x = (CONVEYOR_BASE_SPEED * time.delta_secs()
                             + item.translation.x)
                             .min(con_trans.translation.x + 3.0 * TILE_SIZE / 4.0);
                     }
-                    _ => todo!(),
                 }
+                _ => todo!(),
             }
         }
     }
@@ -293,5 +293,31 @@ mod tests {
         assert_eq!(1, items.len());
         let actual_x = items[0].1.translation.x;
         assert_eq!(actual_x, TILE_SIZE / 2.0 + TILE_SIZE / 4.0);
+    }
+
+    #[test]
+    fn conveyor_doesnt_moves_item_just_before_start() {
+        let mut app = test_app();
+
+        let world = app.world_mut();
+        let item_entity = world.spawn_empty().id();
+        // almost at the edge of the conveyor
+        let x = -1.0 / 2.0 - 1.0 / 4.0;
+        world.write_message(CreateWorldItem(item_entity, WorldCoords { x, y: 0.0 }));
+        let conveyor_entity = world.spawn_empty().id();
+        world.write_message(CreateConveyor(
+            conveyor_entity,
+            Vec2 { x: 0.0, y: 0.0 },
+            Direction::East,
+        ));
+
+        app.update();
+        app.update();
+
+        let mut query = app.world_mut().query::<(&WorldItem, &Transform)>();
+        let items = query.iter(app.world()).collect::<Vec<_>>();
+        assert_eq!(1, items.len());
+        let actual_x = items[0].1.translation.x;
+        assert_eq!(actual_x, -TILE_SIZE / 2.0 - TILE_SIZE / 4.0);
     }
 }
